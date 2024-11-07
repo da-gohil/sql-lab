e7761789c587f897710d8ae905626e373c71b8fb
da-gohil <157419078+da-gohil@users.noreply.github.com>
2024-11-07 11:55:58 -0500

    Update Bronze1.sql

--- HackerRank/Bronze1.sql
+++ HackerRank/Bronze1.sql
@@ -1,1 +1,22 @@
 -- Query 01
+/*
+Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+*/
+
+SELECT *
+FROM CITY
+WHERE CountryCode = 'USA' AND Population > 100000;
+
+-- Query 02
+/*
+Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+*/
+SELECT NAME 
+FROM CITY
+WHERE COUNTRYCODE ='USA' AND POPULATION > 120000;
+
+-- Quering all columns
+SELECT * FROM CITY;
+
+
+
